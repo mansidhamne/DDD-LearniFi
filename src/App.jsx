bc53d32d740555160ddb6f8f8977be7c541c030d
Parth Chitre <127012206+PChitre07@users.noreply.github.com>
2024-01-21 22:16:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import "./app.scss";
 import { Navbar } from "./components/Navbar";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import { Landing } from "./pages/Landing";
+import LoginForm from "./pages/LoginForm";
 const App = () => {
   return <div>
-    
+    <LoginForm />
     <Navbar />
     <Router>
       <Routes>
